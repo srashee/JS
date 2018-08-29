5ba3929b462d1dc0a6c6a03ff88886de8f618a3f
Saad Rasheed <saarasheed@outlook.com>
2018-08-28 19:26:01 -0500

    improving project to host actual web app

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  //res.send('Hello World!');
+  res.render('index');
 });
+
+app.post('/', function(req, res) {
+  res.render('success');
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
